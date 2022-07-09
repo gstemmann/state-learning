f76b5ae6dc83bac25ffdb61fabf40e2e7dd37afb
george <george.stemmann@gmail.com>
2022-07-08 23:31:05 -0700

    new and extra forms

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,23 +1,57 @@
 import React, { useState } from 'react';
 
-
 const UserForm = () => {
-    const [name, changeName] = useState('blue man');
-    const handleChange = (e) => {
-        changeName(e.target.value)
+    const initialState = {
+        username: '',
+        email: '',
+        password: ''
     }
-    const handleSubmit = () => {
+    const [formData, changeFormData] = useState(initialState);
+    const handleChangeOfState = e => {
+        const { name, value } = e.target;
+        changeFormData(data => ({
+            ...data,
+            [name]: value
+        }))
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const { username, email, password } = formData;
+        alert(`created user: ${username} ${email} ${password}`)
+        changeFormData(initialState);
         
     }
 
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input 
+                id="name"
+                name="username"
                 type="text" 
-                placeholder="Name" 
-                value={name} 
-                onChange={handleChange} />
-            <button onClick={handleSubmit}> add </button>
+                placeholder="username" 
+                value={formData.username} 
+                onChange={handleChangeOfState}
+            />
+
+            <input 
+                id="email"
+                name="email"
+                type="text"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChangeOfState}
+            />
+            <input 
+                id="password"
+                name="password"
+                type="text"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChangeOfState}
+            />
+            <button> add </button>
         </form>
     );
 }
